eb384b901234a408e1a1afaf0dab0ff6b231f21b
TasmiaRaisaHasan <raisahasan80@gmail.com>
2022-12-08 01:26:21 +0600

    day 2 files

---
+++ Day 2_07.12.2022/Tasmia/dictionary.py
@@ -0,0 +1,21 @@
+dictionary= {1: 'dhaka', 2:'jashore', 3: 'rangpur', 4:'cumilla'}
+""" print(dictionary.get(2))
+dictionary[5]='chittagong'
+print(dictionary)
+dictionary[5]='chottogram'
+print(dictionary)
+print(dictionary.clear())
+copy=dictionary.copy()
+print(copy)
+print(dictionary.update()) 
+print(dictionary.values()) 
+print(dictionary.get(3))
+print(dictionary.items())
+print(dictionary.keys())
+print(dictionary.popitem()) """
+
+dictionary1= dict(key1='hello', key2='world')
+print(dictionary1)
+
+a= {1:2, 3:4}
+print(a.fromkeys(dictionary1))
